665b6911739fdbcbb01b0c259c805a507770c8df
adnanrais3110 <adnan.rais4110@gmail>
2023-11-03 16:56:12 +0700

    updatetugas

--- src/id/ac/unpas/ib/latihan/heuristik/astar/MainTest.java
+++ src/id/ac/unpas/ib/latihan/heuristik/astar/MainTest.java
@@ -5,9 +5,9 @@ public class MainTest {
     // fungsi main
     public static void main(String[] args) {
         // inisialisasi node-node yang ada
-        Node Cilegon = new Node("Cilegon");
-        Node Serang = new Node("Serang");
-        Node Tanggerang = new Node("Tanggerang");
+        Node Pangkalpinang = new Node("Pangkalpinang");
+        Node Probolinggo = new Node("Probolinggo");
+        Node Pasuruan = new Node("Pasuruan");
         Node Jakarta = new Node("Jakarta");
         Node Bekasi = new Node("Bekasi");
         Node Bandung = new Node("Bandung");
@@ -16,20 +16,20 @@ public class MainTest {
         Node Semarang = new Node("Semarang");
         Node Surabaya = new Node("Surabaya");
         Node Malang = new Node("Malang");
-        Node Palembang = new Node("Palembang");
+        Node Madiun = new Node("Madiun");
         Node Padang = new Node("Padang");
         Node Pekanbaru = new Node("Pekanbaru");
         Node Jambi = new Node("Jambi");
         Node Medan = new Node("Medan");
         Node Kalimantan = new Node("Kalimantan");
         Node Batam = new Node("Batam");
-        Node Bengkulu = new Node("Bengkulu");
+        Node Tegal = new Node("Tegal");
         Node Bali = new Node("Bali");
 
         // inisialisasi node-node yang ada dengan costnya
-        NodeUCS CilegonUCS = new NodeUCS(Cilegon, 366);
-        NodeUCS SerangUCS = new NodeUCS(Serang, 0);
-        NodeUCS TanggerangUCS = new NodeUCS(Tanggerang, 160);
+        NodeUCS PangkalpinangUCS = new NodeUCS(Pangkalpinang, 366);
+        NodeUCS ProbolinggoUCS = new NodeUCS(Probolinggo, 0);
+        NodeUCS PasuruanUCS = new NodeUCS(Pasuruan, 160);
         NodeUCS JakartaUCS = new NodeUCS(Jakarta, 242);
         NodeUCS BekasiUCS = new NodeUCS(Bekasi, 161);
         NodeUCS BandungUCS = new NodeUCS(Bandung, 176);
@@ -38,45 +38,45 @@ public class MainTest {
         NodeUCS SemarangUCS = new NodeUCS(Semarang, 226);
         NodeUCS SurabayaUCS = new NodeUCS(Surabaya, 244);
         NodeUCS MalangUCS = new NodeUCS(Malang, 241);
-        NodeUCS PalembangUCS = new NodeUCS(Palembang, 234);
+        NodeUCS MadiunUCS = new NodeUCS(Madiun, 234);
         NodeUCS PadangUCS = new NodeUCS(Padang, 380);
         NodeUCS PekanbaruUCS = new NodeUCS(Pekanbaru, 10);
         NodeUCS JambiUCS = new NodeUCS(Jambi, 193);
         NodeUCS MedanUCS = new NodeUCS(Medan, 253);
         NodeUCS KalimantanUCS = new NodeUCS(Kalimantan, 329);
         NodeUCS BatamUCS = new NodeUCS(Batam, 80);
-        NodeUCS BengkuluUCS = new NodeUCS(Bengkulu, 199);
+        NodeUCS TegalUCS = new NodeUCS(Tegal, 199);
         NodeUCS BaliUCS = new NodeUCS(Bali, 374);
 
         // menambahkan tetangga dari masing-masing node dengan costnya
-        CilegonUCS.addTetangga(BaliUCS, 75);
-        CilegonUCS.addTetangga(MedanUCS, 140);
-        CilegonUCS.addTetangga(KalimantanUCS, 118);
+        PangkalpinangUCS.addTetangga(BaliUCS, 75);
+        PangkalpinangUCS.addTetangga(MedanUCS, 140);
+        PangkalpinangUCS.addTetangga(KalimantanUCS, 118);
 
-        SerangUCS.addTetangga(BandungUCS, 211);
-        SerangUCS.addTetangga(PekanbaruUCS, 101);
-        SerangUCS.addTetangga(CirebonUCS, 90);
-        SerangUCS.addTetangga(BatamUCS, 85);
+        ProbolinggoUCS.addTetangga(BandungUCS, 211);
+        ProbolinggoUCS.addTetangga(PekanbaruUCS, 101);
+        ProbolinggoUCS.addTetangga(CirebonUCS, 90);
+        ProbolinggoUCS.addTetangga(BatamUCS, 85);
 
-        TanggerangUCS.addTetangga(JakartaUCS, 120);
-        TanggerangUCS.addTetangga(JambiUCS, 146);
-        TanggerangUCS.addTetangga(PekanbaruUCS, 138);
+        PasuruanUCS.addTetangga(JakartaUCS, 120);
+        PasuruanUCS.addTetangga(JambiUCS, 146);
+        PasuruanUCS.addTetangga(PekanbaruUCS, 138);
 
         JakartaUCS.addTetangga(MalangUCS, 75);
-        JakartaUCS.addTetangga(TanggerangUCS, 120);
+        JakartaUCS.addTetangga(PasuruanUCS, 120);
 
         BekasiUCS.addTetangga(IndramayuUCS, 86);
 
         BandungUCS.addTetangga(MedanUCS, 99);
-        BandungUCS.addTetangga(SerangUCS, 211);
+        BandungUCS.addTetangga(ProbolinggoUCS, 211);
 
-        CirebonUCS.addTetangga(SerangUCS, 90);
+        CirebonUCS.addTetangga(ProbolinggoUCS, 90);
 
         IndramayuUCS.addTetangga(BatamUCS, 98);
         IndramayuUCS.addTetangga(BekasiUCS, 86);
 
-        SemarangUCS.addTetangga(PalembangUCS, 87);
-        SemarangUCS.addTetangga(BengkuluUCS, 92);
+        SemarangUCS.addTetangga(MadiunUCS, 87);
+        SemarangUCS.addTetangga(TegalUCS, 92);
 
         SurabayaUCS.addTetangga(KalimantanUCS, 111);
         SurabayaUCS.addTetangga(MalangUCS, 70);
@@ -84,43 +84,44 @@ public class MainTest {
         MalangUCS.addTetangga(SurabayaUCS, 70);
         MalangUCS.addTetangga(JakartaUCS, 75);
 
-        PalembangUCS.addTetangga(SemarangUCS, 87);
+        MadiunUCS.addTetangga(SemarangUCS, 87);
 
         PadangUCS.addTetangga(BaliUCS, 71);
         PadangUCS.addTetangga(MedanUCS, 151);
 
         PekanbaruUCS.addTetangga(JambiUCS, 97);
-        PekanbaruUCS.addTetangga(TanggerangUCS, 138);
-        PekanbaruUCS.addTetangga(SerangUCS, 101);
+        PekanbaruUCS.addTetangga(PasuruanUCS, 138);
+        PekanbaruUCS.addTetangga(ProbolinggoUCS, 101);
 
         JambiUCS.addTetangga(MedanUCS, 80);
-        JambiUCS.addTetangga(TanggerangUCS, 146);
+        JambiUCS.addTetangga(PasuruanUCS, 146);
         JambiUCS.addTetangga(PekanbaruUCS, 97);
 
-        MedanUCS.addTetangga(CilegonUCS, 140);
+        MedanUCS.addTetangga(PangkalpinangUCS, 140);
         MedanUCS.addTetangga(PadangUCS, 151);
         MedanUCS.addTetangga(BandungUCS, 99);
         MedanUCS.addTetangga(JambiUCS, 80);
 
-        KalimantanUCS.addTetangga(CilegonUCS, 118);
+        KalimantanUCS.addTetangga(PangkalpinangUCS, 118);
         KalimantanUCS.addTetangga(SurabayaUCS, 111);
 
-        BatamUCS.addTetangga(SerangUCS, 85);
-        BatamUCS.addTetangga(BengkuluUCS, 142);
+        BatamUCS.addTetangga(ProbolinggoUCS, 85);
+        BatamUCS.addTetangga(TegalUCS, 142);
         BatamUCS.addTetangga(BaliUCS, 98);
 
-        BengkuluUCS.addTetangga(SemarangUCS, 92);
-        BengkuluUCS.addTetangga(BatamUCS, 142);
+        TegalUCS.addTetangga(SemarangUCS, 92);
+        TegalUCS.addTetangga(BatamUCS, 142);
 
-        BaliUCS.addTetangga(CilegonUCS, 75);
+        BaliUCS.addTetangga(PangkalpinangUCS, 75);
         BaliUCS.addTetangga(PadangUCS, 71);
 
         // menampilkan hasil dari algoritma A*
         System.out.println("A*");
         // inisialisasi objek AStar
         AStar aStar = new AStar();
-        // memanggil fungsi search dari objek aStar dari node CilegonUCS ke SerangUCS
-        aStar.search(CilegonUCS, SerangUCS);
+        // memanggil fungsi search dari objek aStar dari node PangkalpinangUCS ke
+        // ProbolinggoUCS
+        aStar.search(PangkalpinangUCS, ProbolinggoUCS);
         System.out.println();
     }
 }
